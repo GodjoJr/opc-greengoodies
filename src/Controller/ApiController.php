3e9dc1f40b7723d3da2cf145a0cc7f43515328ce
Sebastien Houssein <housseinseb@icloud.com>
2026-06-20 13:25:57 +0200

    Adding profile style + fix api

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,26 +4,32 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 #[Route('/api', name: 'app_api_')]
 final class ApiController extends AbstractController
 {
-#[Route('/products', name: 'products', methods: ['GET'])]
-    public function showProducts(ProductRepository $productRepository): Response
+
+    #[Route('/products', name: 'products', methods: ['GET'])]
+    public function showProducts(ProductRepository $productRepository, SerializerInterface $serializer): Response
     {
-       $products = $productRepository->findAll();
+        $products = $productRepository->findAll();
 
-        $data = array_map(fn($p) => [
-            'id'               => $p->getId(),
-            'name'             => $p->getName(),
-            'shortDescription' => $p->getShortDescription(),
-            'fullDescription'  => $p->getFullDescription(),
-            'price'            => $p->getPrice(),
-            'picture'          => $p->getPicture(),
-        ], $products);
+        $json = $serializer->serialize($products, 'json', [
+            AbstractNormalizer::ATTRIBUTES => [
+                'id',
+                'name',
+                'shortDescription',
+                'fullDescription',
+                'price',
+                'picture',
+            ],
+        ]);
 
-        return $this->json($data);
+        return new JsonResponse($json, 200, [], true);
     }
 }
